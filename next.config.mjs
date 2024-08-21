31b51ab42f332f176e73ea992555799a517bdb99
Hudayar-T <tash.hudayar@gmail.com>
2024-08-21 18:11:29 +0300

    implement price updating in tmt

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts'],
+  i18n: {
+    locales: ['en', 'ru', 'tk', 'ch'],
+    defaultLocale: 'ru',
+    localeDetection: false,
+  },
+  images: {
+    domains: ['https://presumably-patient-yak.ngrok-free.app'],
+  },
+};
+
+export default nextConfig;
