64461e0775085518101b777f6d2e59fd00ed9a6d
Hudayar-T <tash.hudayar@gmail.com>
2024-08-21 19:26:02 +0300

    fix add highest level cat

---
+++ src/pages/components/Drawer.tsx
@@ -0,0 +1,129 @@
+import Collapsable from '@/pages/components/Collapsable';
+import { useCategoryContext } from '@/pages/lib/CategoryContext';
+import {
+  appBarHeight,
+  drawerPaddingTopOffset,
+  HIGHEST_LEVEL_CATEGORY_ID,
+  MAIN_BG_COLOR,
+  mobileAppBarHeight,
+} from '@/pages/lib/constants';
+import {
+  DeleteCategoriesProps,
+  EditCategoriesProps,
+  ExtendedCategory,
+} from '@/pages/lib/types';
+import { useUserContext } from '@/pages/lib/UserContext';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import { IconButton, List, Paper, Tooltip } from '@mui/material';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import * as React from 'react';
+import { Dispatch, SetStateAction } from 'react';
+
+const drawerWidth = 300;
+
+interface CustomDrawerProps {
+  setEditCategoriesModal: Dispatch<SetStateAction<EditCategoriesProps>>;
+  setDeleteCategoriesModal: Dispatch<SetStateAction<DeleteCategoriesProps>>;
+  openDrawer: boolean;
+  closeDrawer: () => void;
+}
+
+function ConstructDrawerList(
+  categories: ExtendedCategory[],
+  selectedCategoryId: string | undefined,
+  setSelectedCategoryId: Dispatch<SetStateAction<string | undefined>>,
+  setEditCategoriesModal: Dispatch<SetStateAction<EditCategoriesProps>>,
+  setDeleteCategoriesModal: Dispatch<SetStateAction<DeleteCategoriesProps>>,
+  depth: number,
+): React.ReactNode {
+  return (
+    <List component="div" disablePadding className="flex flex-col">
+      {categories.map(({ id, imgUrl, name, successorCategories }) => (
+        <Collapsable
+          id={id}
+          categoryTitle={name}
+          imgUrl={imgUrl}
+          key={name}
+          pl={depth}
+          initialOpenState={id === selectedCategoryId}
+          collapsable={
+            successorCategories != null && successorCategories.length > 0
+          }
+          setEditCategoriesModal={setEditCategoriesModal}
+          setDeleteCategoriesModal={setDeleteCategoriesModal}
+        >
+          {ConstructDrawerList(
+            successorCategories!,
+            selectedCategoryId,
+            setSelectedCategoryId,
+            setEditCategoriesModal,
+            setDeleteCategoriesModal,
+            depth + 1,
+          )}
+        </Collapsable>
+      ))}
+    </List>
+  );
+}
+
+export default function CustomDrawer({
+  setEditCategoriesModal,
+  setDeleteCategoriesModal,
+  openDrawer,
+  closeDrawer,
+}: CustomDrawerProps) {
+  const { categories, selectedCategoryId, setSelectedCategoryId } =
+    useCategoryContext();
+  const { user } = useUserContext();
+
+  return (
+    <Drawer
+      variant="temporary"
+      open={openDrawer}
+      sx={{
+        width: drawerWidth,
+        height: '100%',
+        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+      }}
+      onClose={closeDrawer}
+    >
+      {categories?.length > 0 && (
+        <Box
+          sx={{
+            overflow: 'auto',
+            pt: {
+              xs: `${mobileAppBarHeight + drawerPaddingTopOffset}px`,
+              sm: `${appBarHeight + drawerPaddingTopOffset}px`,
+            },
+            height: '100vh',
+          }}
+          className={`bg-[${MAIN_BG_COLOR}]`}
+        >
+          {ConstructDrawerList(
+            categories,
+            selectedCategoryId,
+            setSelectedCategoryId,
+            setEditCategoriesModal,
+            setDeleteCategoriesModal,
+            0, // depth
+          )}
+        </Box>
+      )}
+      {user?.grade === 'ADMIN' && (
+        <Paper className="h-12 w-full absolute bottom-0 bg-slate-100 flex justify-center">
+          <Tooltip title="Edit categories">
+            <IconButton
+              onClick={() => {
+                setSelectedCategoryId(HIGHEST_LEVEL_CATEGORY_ID);
+                setEditCategoriesModal({ open: true, dialogType: 'add' });
+              }}
+            >
+              <AddCircleIcon fontSize="small" color="primary" />
+            </IconButton>
+          </Tooltip>
+        </Paper>
+      )}
+    </Drawer>
+  );
+}
---
+++ src/pages/lib/constants.ts
@@ -0,0 +1,50 @@
+// eslint-disable-next-line import/prefer-default-export
+
+export const PRODUCT_IMAGE_WIDTH = 1024; // px
+
+export const appBarHeight = 64;
+
+export const mobileAppBarHeight = 56;
+
+export const localeOptions = ['en', 'tk', 'ru', 'ch'];
+
+export const drawerPaddingTopOffset = 2;
+
+export const MAIN_BG_COLOR = '#F8F9FA';
+
+export const DEFAULT_MUI_COLOR = '#1976d2';
+
+export const LOGO_COLOR = '#1976d2';
+export const LOGO_COLOR_LIGHT = '#1d72c2';
+export const LOGO_COLOR_ORANGE = '#ff6f00';
+/**
+ * #221765 -> original
+ * #4B4090
+ * #6459A6
+ * #7F74BD
+ * #A89EE0 -> lightest
+ */
+
+export const PRODUCT_IMAGE_WIDTH_RESP = {
+  width: {
+    xs: '75%',
+    sm: '50%',
+    md: '75%',
+  },
+};
+
+export const squareBracketRegex = /\[([^\]]+)\]/;
+
+export const POLL_DOLLAR_RATE_INTERVAL = 600_000; // 10 minutes
+export const POLL_PRODUCT_INTERVAL = 600_000; // 10 minutes
+
+export const HIGHEST_LEVEL_CATEGORY_ID = 'no_parent_category';
+
+export const defaultProductDescEn =
+  '[Release date]\n[Display]\n[Processor]\n[Camera]\n[Battery]\n[OS]';
+export const defaultProductDescTk =
+  '[Çykan wagty]\n[Ekran]\n[Prosessor]\n[Kamera]\n[Batareýka]\n[OS]';
+export const defaultProductDescCh =
+  '[Çykan wagty]\n[Ekran]\n[Prosessor]\n[Kamera]\n[Batareýka]\n[OS]';
+export const defaultProductDescRu =
+  '[Дата выхода]\n[Дисплей]\n[Процессор]\n[Камера]\n[Батарея]\n[OS]';
